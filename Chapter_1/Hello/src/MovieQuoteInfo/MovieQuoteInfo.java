5ef77841694e27067fd84babe7542be819ce5af6
none for nothing <nonefornothing.gmail.com>
2020-03-26 19:01:18 +0700

    menambahkan tugas Movie Quote Info

---
+++ Chapter_1/Hello/src/MovieQuoteInfo/MovieQuoteInfo.java
@@ -0,0 +1,11 @@
+package MovieQuoteInfo;
+
+public class MovieQuoteInfo {
+    public static void main(String[] args) {
+        System.out.println("Behind every successful fortune there is a crime.");
+        System.out.println("The GODFATHER");
+        System.out.println("Don Vito Corleone");
+        System.out.println("1972");
+    }
+
+}
