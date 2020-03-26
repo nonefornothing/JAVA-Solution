75a61dacf1a88711a35a5eadb000b3e9912a2775
none for nothing <nonefornothing.gmail.com>
2020-03-26 15:38:25 +0700

    menambahkan program hello world

---
+++ Hello_World/Hello/src/JOX/App.java
@@ -0,0 +1,7 @@
+package JOX;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello Java");
+    }
+}
---
+++ Hello_World/Hello/src/app/FirstDialog.java
@@ -0,0 +1,9 @@
+package app;
+
+import javax.swing.JOptionPane;
+
+public class FirstDialog {
+    public static void main(final String[] args) {
+        JOptionPane.showMessageDialog(null, "First Java dialog for who ?");
+    }
+}
