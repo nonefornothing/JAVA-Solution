ded6f6be7f12621edf394fd65a1996c28e021140
none for nothing <nonefornothing.gmail.com>
2020-03-26 17:48:48 +0700

    merekonstruksi project

---
+++ Chapter_1/Hello/bin/SongLyrics/SongLyrics.java
@@ -0,0 +1,38 @@
+package SongLyrics;
+
+public class SongLyrics {
+    public static void main(String[] args) {
+        System.out.println("Living easy, living free Season ticket on a one-way ride");
+        System.out.println("Asking nothing, leave me be");
+        System.out.println("Taking everything in my stride");
+        System.out.println("Don't need reason, don't need rhyme");
+        System.out.println("Ain't nothing I would rather do");
+        System.out.println("Going down, party time");
+        System.out.println("My friends are gonna be there too");
+        System.out.println("I'm on the highway to hell");
+        System.out.println("On the highway to hell");
+        System.out.println("Highway to hell");
+        System.out.println("I'm on the highway to hell");
+        System.out.println("No stop signs, speed limit");
+        System.out.println("Nobody's gonna slow me down");
+        System.out.println("Like a wheel, gonna spin it");
+        System.out.println("Nobody's gonna mess me around");
+        System.out.println("Hey Satan, paid my dues");
+        System.out.println("Playing in a rocking band");
+        System.out.println("Hey mama, look at me");
+        System.out.println("I'm on my way to the promised land, whoo!");
+        System.out.println("I'm on the highway to hell , Highway to hell");
+        System.out.println("I'm on the highway to hell , Highway to hell");
+        System.out.println("Don't stop me");
+        System.out.println("I'm on the highway to hell, On the highway to hell");
+        System.out.println("I'm on the highway to hell , On the highway");
+        System.out.println("Yeah, highway to hell");
+        System.out.println("I'm on the highway to hell");
+        System.out.println("Highway to hell");
+        System.out.println("Highway to hell");
+        System.out.println("And I'm going down");
+        System.out.println("All the way");
+        System.out.println("Whoa!");
+        System.out.println("I'm on the highway to hell");
+    }
+}
---
+++ Chapter_1/Hello/src/JOX/FirstDialog.java
@@ -0,0 +1,9 @@
+package JOX;
+
+import javax.swing.JOptionPane;
+
+public class FirstDialog {
+    public static void main(final String[] args) {
+        JOptionPane.showMessageDialog(null, "First Java dialog for who ? and by who ?");
+    }
+}
