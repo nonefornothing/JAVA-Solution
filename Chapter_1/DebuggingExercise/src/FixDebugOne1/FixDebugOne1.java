2a1fc95f55af69b7d29eaa32d809488c77b692ef
none for nothing <nonefornothing.gmail.com>
2020-03-27 01:28:12 +0700

    temporary update

---
+++ Chapter_1/DebuggingExercise/src/FixDebugOne1/FixDebugOne1.java
@@ -0,0 +1,8 @@
+package FixDebugOne1;
+
+public class FixDebugOne1 {
+   /* This program displays a greeting */
+   public static void main(String[] args) {
+      System.out.printl("Hello");
+   }
+}
