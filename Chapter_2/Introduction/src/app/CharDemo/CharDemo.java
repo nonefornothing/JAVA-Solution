50672611469bfa88d2a42dcba6920a0e1550e0af
none for nothing <nonefornothing.gmail.com>
2020-03-28 01:26:32 +0700

    add program from chapter 2

---
+++ Chapter_2/Introduction/src/app/CharDemo/CharDemo.java
@@ -0,0 +1,11 @@
+package app.CharDemo;
+
+public class CharDemo {
+
+    public static void main(String[] args) {
+        char initial = 'A';
+
+        System.out.println(initial);
+        System.out.println("\t\"abc\\deeewddffff\bghi\n\njkl");
+    }
+}
---
+++ Chapter_2/Introduction/src/app/NumbersPrintln/NumbersPrintln.java
@@ -0,0 +1,10 @@
+package app.NumbersPrintln;
+
+public class NumbersPrintln {
+    public static void main(String[] args) {
+        int billingDate = 5;
+
+        System.out.println("Bill are sent on day " + billingDate + " of the month");
+        System.out.println("Next bill: October " + billingDate);
+    }
+}
