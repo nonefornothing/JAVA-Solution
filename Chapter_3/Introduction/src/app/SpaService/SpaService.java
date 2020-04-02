9d99df68fe8e246a36518bfaa728e1c069b089ad
none for nothing <nonefornothing.gmail.com>
2020-04-02 07:18:22 +0700

    .

---
+++ Chapter_3/Introduction/src/app/SpaService/SpaService.java
@@ -0,0 +1,29 @@
+package app.SpaService;
+
+public class SpaService {
+
+    private String serviceDescription;
+    private double price;
+
+    public SpaService() {
+        serviceDescription = "XXX";
+        price = 0;
+    }
+
+    public void setServiceDescription(String service) {
+        serviceDescription = service;
+    }
+
+    public void setPrice(double servicePrice) {
+        price = servicePrice;
+    }
+
+    public String getServiceDescription() {
+        return serviceDescription;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+}
